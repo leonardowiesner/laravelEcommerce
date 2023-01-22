874de03b26d853ac1636b611d842392a4aa8f587
Leonardo Wiesner <leonardowiesner11@gmail.com>
2023-01-22 23:45:45 +0100

    Seeders para los productos

---
+++ app/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
+
+class Product extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'stock',
+        'status'
+
+    ];
+}
